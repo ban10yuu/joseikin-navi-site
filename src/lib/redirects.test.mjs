a193fe44bd87bf7fc39cb2b47cce8d7c370187ae
番正　優児 <ban10yuu@gmail.com>
2026-07-22 12:17:32 +0900

    深谷市制度URLを正規化してPV計測を有効化

--- src/lib/redirects.test.mjs
+++ src/lib/redirects.test.mjs
@@ -9,6 +9,7 @@ describe('grant redirects', () => {
       { source: '/grant/kushiro-elderly-taxi/', destination: '/grant/kushiro-elderly-outing-bus/', statusCode: 301 },
       { source: '/grant/kagoshima-elderly-taxi/', destination: '/grant/kagoshima-keiro-pass/', statusCode: 301 },
       { source: '/grant/fukuyama-elderly-taxi/', destination: '/grant/fukuyama-elderly-support/', statusCode: 301 },
+      { source: '/grant/fukaya-結婚新生活支援事業補助金lywed-life-support-subsidy-2026/', destination: '/grant/fukaya-newlywed-life-support-subsidy-2026/', statusCode: 301 },
     ]);
   });
 
@@ -19,7 +20,7 @@ describe('grant redirects', () => {
   it('旧URLのslugを掲載データから除外できる', () => {
     assert.deepEqual(
       [...REDIRECT_SOURCE_SLUGS],
-      ['kushiro-elderly-taxi', 'kagoshima-elderly-taxi', 'fukuyama-elderly-taxi'],
+      ['kushiro-elderly-taxi', 'kagoshima-elderly-taxi', 'fukuyama-elderly-taxi', 'fukaya-結婚新生活支援事業補助金lywed-life-support-subsidy-2026'],
     );
   });
 });
